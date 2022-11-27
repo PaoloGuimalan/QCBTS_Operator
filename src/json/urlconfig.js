6af74405b21a46dc8eb624abc9b434d67b140d3f
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2022-11-26 22:08:55 -0900

    Cyclic url for server initialization

--- src/json/urlconfig.js
+++ src/json/urlconfig.js
@@ -1,2 +1,3 @@
-export const URL = 'https://qcbtsserver.herokuapp.com';
+// export const URL = 'https://qcbtsserver.herokuapp.com';
+export const URL = 'https://qcbtsserver.cyclic.app';
 // export const URL = 'http://localhost:3001';
