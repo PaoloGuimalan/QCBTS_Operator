e7a28f5a5cf1349683433832fdd0c8e41140f694
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2022-11-27 03:30:04 -0900

    Codespaces link for server side testing

--- src/json/urlconfig.js
+++ src/json/urlconfig.js
@@ -1,3 +1,4 @@
 // export const URL = 'https://qcbtsserver.herokuapp.com';
-export const URL = 'https://qcbtsserver.cyclic.app';
+// export const URL = 'https://qcbtsserver.cyclic.app';
 // export const URL = 'http://localhost:3001';
+export const URL = 'https://paologuimalan-ideal-meme-4rxxgq7px4g25j9v-3001.preview.app.github.dev';
